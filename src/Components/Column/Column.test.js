2924e90b1b0417e1c9d568b80ccc947e8430c5ec
Kalyani <kalyanichow8@gmail.com>
2022-10-03 20:43:40 +0530

    issue with husky configuration

---
+++ src/Components/Column/Column.test.js
@@ -0,0 +1,26 @@
+/* eslint-disable no-undef */
+/* eslint-disable react/no-children-prop */
+import { render, screen } from "@testing-library/react";
+import { Column } from "./Column";
+
+test("Column component", () => {
+	render(
+		<Column dataTestId="test-column">
+			<p>{["one", "two"]}</p>
+		</Column>
+	);
+
+	expect(screen.getByTestId("test-column").textContent).toBe("onetwo");
+});
+
+test("Snapshot for Column component", () => {
+	render(
+		<Column
+			children
+			label="test"
+			dataTestId="test-column"
+		/>
+	);
+
+	expect(screen.getByTestId("test-column").textContent).toMatchObject;
+});
